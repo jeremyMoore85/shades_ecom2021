601d9c30bcc1ef6bb67135834b71d71ce1d0b566
Jeremy Moore <40700915+jeremyMoore85@users.noreply.github.com>
2022-06-14 11:34:19 -0700

    Update header.php

--- header.php
+++ header.php
@@ -278,13 +278,13 @@ jQuery(function () {
         <div class="callbacks_container">
           <ul class="rslides" id="slider4">
 			<li> <img src="/blog/wp-content/uploads/2022/05/aniversary-michelle-main.jpg" alt="Shades of Sleep - Cambridge" />
-				<div class="caption"><span class="slide_txt3">Enjoy our 13 year anniversary sale!</span><span class="slide_txt4">In store and on line everything is 13% off from June 9 to June 12, 2022.</span></div>
+				<div class="caption"><span class="slide_txt3">Sleep Well Stay Well</span></div>
             </li>
             <li> <img src="/blog/wp-content/uploads/2022/05/wrap-up-banner.jpg" alt="Shades of Sleep - Cabana" />
-				<div class="caption"><span class="slide_txt3">Enjoy our 13 year anniversary sale!</span><span class="slide_txt4">In store and on line everything is 13% off from June 9 to June 12, 2022.</span></div>
+				<div class="caption"><span class="slide_txt3">Sleep Well Stay Well</span></div>
             </li>
 			<li> <img src="/blog/wp-content/uploads/2022/05/13-aniversary-banner.jpg" alt="Shades of Sleep - Ayrtight" />
-				<div class="caption"><span class="slide_txt3">Enjoy our 13 year anniversary sale!</span><span class="slide_txt4">In store and on line everything is 13% off from June 9 to June 12, 2022.</span></div>
+				<div class="caption"><span class="slide_txt3">Sleep Well Stay Well</span></div>
             </li>
           </ul>
         </div>
